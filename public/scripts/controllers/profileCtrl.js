ea85946de0de529c3ec77fd9e9a86db68ae54425
Shrey <shrey.chowdhary1@gmail.com>
2017-01-30 22:20:14 -0500

    Material

--- public/scripts/controllers/profileCtrl.js
+++ public/scripts/controllers/profileCtrl.js
@@ -1,12 +1,11 @@
 angular.module('splash')
 .controller('profileCtrl', function($scope, profileDataService){
 
-  profileDataService.getProfile(function(response){
-    var profile = response.data.profile;
-    console.log(profile);
-    $scope.profile =  profile;
+    profileDataService.getProfileData(function(response){
+        var profile = response.data.profile;
+        console.log(profile);
+        $scope.profile =  profile;
     });
-    profileDataService.getProfileData($scope.updateProfileData);
     $scope.eliminate = function(){
         profileDataService.eliminate($scope.eliminate,$scope.updateProfileData);
     };
